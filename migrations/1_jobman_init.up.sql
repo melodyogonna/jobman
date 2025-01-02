20d6cb3f85114ac47f20aff4ff6b93ce499b7994
Melody Daniel <melodyogonna@gmail.com>
2025-01-02 14:36:37 +0100

    Create table inside a schema

--- migrations/1_jobman_init.up.sql
+++ migrations/1_jobman_init.up.sql
@@ -1,10 +1,11 @@
-CREATE TYPE jobman_L1nsxfVZfj_status AS ENUM('PENDING', 'RUNNING', 'FINISHED');
-CREATE TABLE IF NOT EXISTS jobman_L1nsxfVZfj(
-  id serial PRIMARY KEY,
-  job_type VARCHAR(255) NOT NULL,
-  data JSONB,
-  due_on TIMESTAMP NOT NULL,
-  completed_on TIMESTAMP,
-  job_status jobman_L1nsxfVZfj_status DEFAULT 'PENDING',
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+CREATE SCHEMA IF NOT EXISTS jobman_L1nsxfVZfj
+  CREATE TYPE jobman_status AS ENUM('PENDING', 'RUNNING', 'FINISHED')
+  CREATE TABLE IF NOT EXISTS jobman(
+    id serial PRIMARY KEY,
+    job_type VARCHAR(255) NOT NULL,
+    data JSONB,
+    due_on TIMESTAMP NOT NULL,
+    completed_on TIMESTAMP,
+    job_status jobman_status DEFAULT 'PENDING',
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  );

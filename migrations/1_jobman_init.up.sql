30cebd5c4668eb9c2b6183410edc6fc5a85fc05c
Melody Daniel <melodyogonna@gmail.com>
2025-01-03 11:37:26 +0100

    Try to make schema definitions separate

--- migrations/1_jobman_init.up.sql
+++ migrations/1_jobman_init.up.sql
@@ -1,11 +1,11 @@
-CREATE SCHEMA IF NOT EXISTS jobman_L1nsxfVZfj
-  CREATE TYPE jobman_status AS ENUM('PENDING', 'RUNNING', 'FINISHED')
-  CREATE TABLE IF NOT EXISTS jobman(
+CREATE SCHEMA IF NOT EXISTS jobman_L1nsxfVZfj;
+  CREATE TYPE jobman_L1nsxfVZfj.jobman_status AS ENUM('PENDING', 'RUNNING', 'FINISHED');
+  CREATE TABLE IF NOT EXISTS jobman_L1nsxfVZfj.jobman(
     id serial PRIMARY KEY,
     job_type VARCHAR(255) NOT NULL,
     data JSONB,
     due_on TIMESTAMP NOT NULL,
     completed_on TIMESTAMP,
-    job_status jobman_status DEFAULT 'PENDING',
+    job_status jobman_L1nsxfVZfj.jobman_status DEFAULT 'PENDING',
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
   );
